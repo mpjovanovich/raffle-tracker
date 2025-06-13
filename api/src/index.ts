0dc78e6a66cb7ee5bdd1d739c5e45007877098ef
Jonny Danger <mpjovanovich@gmail.com>
2025-06-13 16:38:02 -0400

    use config file for port

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,8 @@
 import app from './app.js';
+import { config } from './config/config.js';
 
 // Start the server
-const port = process.env.PORT || 3001;
+const port = config.port;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
