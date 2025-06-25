4331863b04c624c266bdfdef3121e534b5c59ec7
Jonny Danger <mpjovanovich@gmail.com>
2025-06-25 12:26:22 -0400

    Fix auth commented out.

--- web/src/app/events/page.tsx
+++ web/src/app/events/page.tsx
@@ -1,10 +1,12 @@
 import { getEventsAction } from '@/app/actions/events';
 import EventsPage from '@/components/page/events/EventsPage';
+import { requireAuth } from '@/utils/cookieUtility';
+import { ROLE } from '@raffle-tracker/dto';
 
 export default async function Page() {
   // TODO: Viewer roles should not be able to edit stuff.
   // Probably won't have time to flush out viewer.
-  // await requireAuth([ROLE.CASHIER, ROLE.EVENT_MANAGER, ROLE.SELLER]);
+  await requireAuth([ROLE.CASHIER, ROLE.EVENT_MANAGER, ROLE.SELLER]);
   const events = await getEventsAction();
 
   return <EventsPage events={events} />;
