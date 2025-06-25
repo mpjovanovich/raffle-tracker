c6626e71ec66f366a9efe273c5bb376b136384d6
Mike Jovanovich <mpjovanovich@gmail.com>
2025-06-25 16:46:48 -0400

    Dont access login page if logged in.

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,7 +1,12 @@
 import LoginPage from '@/components/page/login/LoginPage';
-
-// TODO: User should not be here if they are logged in.
+import { isLoggedIn } from '@/utils/cookieUtility';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
+  const loggedIn = await isLoggedIn();
+  if (loggedIn) {
+    redirect('/');
+  }
+
   return <LoginPage />;
 }
