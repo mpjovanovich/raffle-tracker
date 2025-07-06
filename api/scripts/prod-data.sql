79a49ccb23c7d69c92681fd592f0f70ecce4eb56
Mike Jovanovich <mpjovanovich@gmail.com>
2025-07-06 14:33:59 -0400

    fix typo

--- api/scripts/prod-data.sql
+++ api/scripts/prod-data.sql
@@ -8,7 +8,7 @@ VALUES
 
 INSERT INTO User (username, password, email, verified)
 VALUES 
-    ('admin', '$2b$10$Ssm85UHNv4K1pcO56sDPS.FJrJpcm2gVGkFtib7mZfYobABPnkQRa', 'admin@example.com', 1),
+    ('admin', '$2b$10$Ssm85UHNv4K1pcO56sDPS.FJrJpcm2gVGkFtib7mZfYobABPnkQRa', 'admin@example.com', 1);
 
 -- Prisma will auto-generate a junction table with this name
 INSERT INTO "_UserToRole" ("A", "B")
