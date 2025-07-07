86a7b05460f490f844c1531ce74a6f56ef7aa0ae
Mike Jovanovich <mpjovanovich@gmail.com>
2025-07-07 19:27:39 -0400

    update dev build to use local database

--- api/src/routes/dev.ts
+++ api/src/routes/dev.ts
@@ -15,33 +15,4 @@ router.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-// // Set temp token for user by ID
-// router.route('/users/:id/setTempToken').get(
-//   asyncHandler(async (req: Request, res: Response) => {
-//     const userId = parseInt(req.params.id);
-
-//     const user = await prisma.user.findUnique({
-//       where: { id: userId },
-//     });
-//     if (!user) {
-//       res.status(404).json(new APIResponse(404, null, 'User not found'));
-//       return;
-//     }
-
-//     const newToken = await generateToken(user.id, TOKEN_TYPE.TEMP);
-//     const updatedUser = await prisma.user.update({
-//       where: { id: user.id },
-//       data: { token: newToken },
-//     });
-
-//     res.status(200).json(
-//       new APIResponse(200, {
-//         token: updatedUser.token,
-//         userId: updatedUser.id,
-//         username: updatedUser.username,
-//       })
-//     );
-//   })
-// );
-
 export default router;
