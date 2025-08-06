2ae82cda298f675705b566e8aa1accd87ca3137e
Mike Jovanovich <mpjovanovich@gmail.com>
2025-08-06 15:15:59 -0400

    Change password button added

--- web/src/components/page/users/UserPage.tsx
+++ web/src/components/page/users/UserPage.tsx
@@ -1,7 +1,9 @@
 import UserDetails from '@/components/page/users/UserDetails';
 import UserRoles from '@/components/page/users/UserRoles';
 import Card from '@/components/ui/Card';
+import SimpleButton from '@/components/ui/SimpleButton';
 import { RoleListItem, User } from '@raffle-tracker/dto';
+import Link from 'next/link';
 
 interface UserPageProps {
   user: User;
@@ -13,6 +15,14 @@ export default function UserPage({ user, roles }: UserPageProps) {
     <>
       <Card title={`User: ${user.username}`}>
         <UserDetails user={user} />
+        <Link href={`/users/${user.id}/changePassword`}>
+          <SimpleButton
+            className="mt-4"
+            title="Change Password"
+          >
+            Change Password
+          </SimpleButton>
+        </Link>
       </Card>
       <UserRoles
         user={user}
