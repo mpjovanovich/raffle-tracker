a032769a23444b42417357eeb265ae0eb7dfef05
Mike Jovanovich <mpjovanovich@gmail.com>
2025-08-08 18:28:25 -0400

    Took note of our terrible security practices.

--- api/scripts/prod-data.sql
+++ api/scripts/prod-data.sql
@@ -6,6 +6,9 @@ VALUES
     ('SELLER'),
     ('VIEWER');
 
+-- Terrible security practice (but we're doing it anyway)!
+-- THIS USER SHOULD ABSOLUTELY BE REMOVED AFTER SETUP!
+-- Any active devs should be added as admin users.
 INSERT INTO User (username, password)
 VALUES 
     ('RT_Admin', '$2y$10$b1qZ0292xxkEBE2ryGaVpe1J.bzuHV7ZYjaAdHCZJSCS3V8ygiNfe');
